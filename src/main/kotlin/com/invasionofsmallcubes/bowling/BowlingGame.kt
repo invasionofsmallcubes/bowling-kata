c4af19f281a6828a1a7ffc5edfa725883d5303e6
Emanuele Ianni <emanuele.ianni@lastminute.com>
2018-02-20 11:04:46 +0100

    refactored last frame

--- src/main/kotlin/com/invasionofsmallcubes/bowling/BowlingGame.kt
+++ src/main/kotlin/com/invasionofsmallcubes/bowling/BowlingGame.kt
@@ -2,12 +2,15 @@ package com.invasionofsmallcubes.bowling
 
 class BowlingGame {
 
+    private val EMPTY_VALUE = ""
+    private val BY_SPACE = " "
+
     fun evaluateScore(sequenceOfRolls: String): Int {
 
         val frames = sequenceOfRolls
-                .split(" ")
+                .split(BY_SPACE)
 
-        return (frames + "")
+        return (frames + EMPTY_VALUE)
                 .zipWithNext()
                 .map { (curr, next) -> FrameFactory.createFrame(curr, next) }
                 .fold(0, { acc, frame -> acc + frame.score() })
--- src/main/kotlin/com/invasionofsmallcubes/bowling/FrameFactory.kt
+++ src/main/kotlin/com/invasionofsmallcubes/bowling/FrameFactory.kt
@@ -2,11 +2,6 @@ package com.invasionofsmallcubes.bowling
 
 object FrameFactory {
 
-    private val scoreByDelivery = mapOf('1' to 1, '2' to 2, '3' to 3,
-            '4' to 4, '5' to 5, '6' to 6, '7' to 7, '8' to 8,
-            '9' to 9, 'X' to 10, '/' to 10, '-' to 0
-    )
-
     private const val LAST_FRAME = ""
 
     fun createFrame(currentFrame: String, nextFrame: String) : FrameObject.Frame {
