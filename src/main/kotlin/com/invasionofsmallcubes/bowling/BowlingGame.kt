3bfc7d69f60745809c871a9b5935ae92fe0ae906
Emanuele Ianni <dierre@gmail.com>
2018-02-20 01:02:17 +0100

    added position calculation

--- src/main/kotlin/com/invasionofsmallcubes/bowling/BowlingGame.kt
+++ src/main/kotlin/com/invasionofsmallcubes/bowling/BowlingGame.kt
@@ -27,19 +27,30 @@ class BowlingGame {
     }
 
     private fun getFrame(index: Int, frames: List<String>, it: String, sequenceOfRolls: String): Frame {
+
+        val nextSpareDelivery = if (it.contains("/")) {
+            if (isLastFrame(index, frames, it)) it[2] else sequenceOfRolls[(index + 1) * 3]
+        } else 'a'
+
+        val (nextFirstStrike, nextSecondStirke) = if (it.contains("X")) {
+            if (isLastFrame(index, frames, it)) Pair(it[1], it[2]) else {
+                val position = if (frames[index + 1].length == 2) ((index + 1) * 2) + 1 else ((index + 2) * 2)
+                Pair(sequenceOfRolls[(index + 1) * 2], sequenceOfRolls[position])
+            }
+        } else Pair('a', 'a')
+
         return if (isLastFrame(index, frames, it)) {
             when {
-                it.contains("/") -> Spare(scoreByDelivery[it[2]]!!)
+                it.contains("/") -> Spare(scoreByDelivery[nextSpareDelivery]!!)
                 else -> Strike(listOf(scoreByDelivery[it[1]]!!,
                         scoreByDelivery[it[2]]!!))
             }
         } else {
             when {
-                it.contains("/") -> Spare(scoreByDelivery[sequenceOfRolls[(index + 1) * 3]]!!)
+                it.contains("/") -> Spare(scoreByDelivery[nextSpareDelivery]!!)
                 it.contains("X") -> {
-                    val position = if (frames[index + 1].length == 2) ((index + 1) * 2) + 1 else ((index + 2) * 2)
-                    Strike(listOf(scoreByDelivery[sequenceOfRolls[(index + 1) * 2]]!!,
-                            scoreByDelivery[sequenceOfRolls[position]]!!))
+                    Strike(listOf(scoreByDelivery[nextFirstStrike]!!,
+                            scoreByDelivery[nextSecondStirke]!!))
                 }
                 else -> Normal(it)
             }
@@ -47,5 +58,5 @@ class BowlingGame {
     }
 
     private fun isLastFrame(index: Int, frames: List<String>, it: String) =
-            index == frames.size - 1 && it.length == 3
+            (index == frames.size - 1) && (it.length == 3)
 }
