31186a81b35393f13ec4a663ede2622a2393aa6c
Emanuele Ianni <emanuele.ianni@lastminute.com>
2018-02-20 11:11:12 +0100

    naming and constants

--- src/main/kotlin/com/invasionofsmallcubes/bowling/FrameFactory.kt
+++ src/main/kotlin/com/invasionofsmallcubes/bowling/FrameFactory.kt
@@ -3,16 +3,18 @@ package com.invasionofsmallcubes.bowling
 object FrameFactory {
 
     private const val LAST_FRAME = ""
+    private const val SPARE = "/"
+    private const val STRIKE = "X"
 
-    fun createFrame(currentFrame: String, nextFrame: String) : FrameObject.Frame {
+    fun createFrame(currentFrame: String, nextFrame: String): FrameObject.Frame {
 
-        val next = if(nextFrame == LAST_FRAME) currentFrame else nextFrame
+        val next = if (nextFrame == LAST_FRAME) currentFrame else nextFrame
 
         return when {
-            currentFrame.contains("/") -> {
+            currentFrame.contains(SPARE) -> {
                 Spare(next)
             }
-            currentFrame.contains("X") -> {
+            currentFrame.contains(STRIKE) -> {
                 Strike(next)
             }
             else -> Normal(currentFrame)
